fb298affe6eb57cfa961a3f426a031df511aab77
Prashant Pal <palprashant465@gmail.com>
2021-12-05 20:05:23 +0530

    basic practice

--- firstpro/src/com/prashant/Inputs.java
+++ firstpro/src/com/prashant/Inputs.java
@@ -4,9 +4,17 @@ import java.util.Scanner;
 
 public class Inputs {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);    // System.in will take input from keyboard.
-        int rollno = input.nextInt();
-        System.out.println("your roll no is " + rollno);
-        
+        Scanner input = new Scanner(System.in);            // System.in will take input from keyboard.
+//        System.out.print("please enter your no: ");
+//        int rollno = input.nextInt();
+//        System.out.println("your roll no is " + rollno);
+
+//          String name = input.nextLine();                  // nextLine will print entire string.
+//          System.out.println(name);
+
+//          Float marks = input.nextFloat();
+//          System.out.println(marks);
+
+
     }
 }
