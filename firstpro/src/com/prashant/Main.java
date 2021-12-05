bc2bd2926a4deaa2e6c967ecfc9e632f52fe85ef
Prashant Pal <palprashant465@gmail.com>
2021-12-05 18:23:10 +0530

    basics

--- firstpro/src/com/prashant/Main.java
+++ firstpro/src/com/prashant/Main.java
@@ -1,8 +1,21 @@
 package com.prashant;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;                              //Scanner it is a public class used to take input
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //System.out.print("hello world");              //here it will print without new line
+        //System.out.println("hey");                    //here it will give new line after "hey"
+        Scanner input = new Scanner(System.in);       //System.in to take input from system.
+        //System.out.println(input.nextInt());          // nextInt is used to take integer.
+        System.out.println(input.nextLine());         // nextLine is used for taking string.
+
+
+
+
     }
 }
+
---
+++ firstpro/src/com/prashant/primitives.java
@@ -0,0 +1,4 @@
+package com.prashant;
+
+public class primitives {
+}
