7c04215e3d66b1b8c904f9ac2088e828e9593b90
Prashant Pal <palprashant465@gmail.com>
2021-12-07 00:07:52 +0530

    loops and calculator

---
+++ Calculator/src/com/prashant/Loops.java
@@ -0,0 +1,14 @@
+package com.prashant;
+
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        //Scanner input = new Scanner(System.in);
+
+        for(int num=1; num<=5; num++) {
+            System.out.println(num);
+        }
+
+    }
+}
--- Calculator/src/com/prashant/Main.java
+++ Calculator/src/com/prashant/Main.java
@@ -4,6 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        
+        /*
+       if(expression T or F) {
+           code
+                          }
+       else{
+            code
+       }
+      */
+
+        int salary=25000;
+        if(salary>10000){
+            salary=salary+5000;
+        }
+        else{
+            salary=salary+10000;
+        }
+        System.out.println(salary);
+
     }
 }
